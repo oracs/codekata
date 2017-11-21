32bb59c9a397374dc5c55d46f4f45cee6eb266db
oracs <oracs@163.com>
2017-11-21 20:38:09 +0800

    #countoff#, complete sprint1 requirements

--- countoff/src/main/java/com/dy/kata/countoff/Kids.java
+++ countoff/src/main/java/com/dy/kata/countoff/Kids.java
@@ -3,13 +3,18 @@ package com.dy.kata.countoff;
 public class Kids {
     public String countOff(int number) {
         String result = "";
+
         if (number % 3 == 0) {
-            result = "石头";
-        } else if (number % 5 == 0) {
-            result = "剪刀";
-        } else if (number % 7 == 0) {
-            result = "布";
-        } else {
+            result += "石头";
+        }
+        if (number % 5 == 0) {
+            result += "剪刀";
+        }
+        if (number % 7 == 0) {
+            result += "布";
+        }
+
+        if (result.equals("")) {
             result = String.valueOf(number);
         }
 
