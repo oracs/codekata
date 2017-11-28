185acdee0a22b1af490d7603515e1e5dbde3270d
oracs <oracs@163.com>
2017-11-28 20:07:45 +0800

    #countoff#, add convert algorithm

--- countoff/src/main/java/com/dy/kata/countoff/Kids.java
+++ countoff/src/main/java/com/dy/kata/countoff/Kids.java
@@ -1,11 +1,18 @@
 package com.dy.kata.countoff;
 
 import com.dy.kata.countoff.convertor.NumConvertor;
+import com.dy.kata.countoff.literal.Literal;
 
 public class Kids {
+    private Literal literal;
+
+    public Kids(Literal literal) {
+        this.literal = literal;
+    }
+
     public String countOff(int toNumber) {
         String result = "";
-        NumConvertor convertor = new NumConvertor(3, 5, 7);
+        NumConvertor convertor = new NumConvertor(literal);
         for (int i = 1; i <= toNumber; i++) {
             result += convertor.convert(i) + ",";
         }
--- countoff/src/test/java/com/dy/kata/countoff/convertor/ConvertorTest.groovy
+++ countoff/src/test/java/com/dy/kata/countoff/convertor/ConvertorTest.groovy
@@ -1,12 +1,13 @@
 package com.dy.kata.countoff.convertor
 
+import com.dy.kata.countoff.literal.impl.Literal1
 import spock.lang.Specification
 
 class ConvertorTest extends Specification {
 
-    def "test_convert_special_numbers"() {
+    def "test_convert_special_numbers_357"() {
         given:
-        NumConvertor convertor = new NumConvertor(3, 5, 7)
+        NumConvertor convertor = new NumConvertor(new Literal1(3, 5, 7))
 
         expect:
         result == convertor.convert(number)
@@ -33,5 +34,35 @@ class ConvertorTest extends Specification {
         105    || "石头剪刀布"
     }
 
+    def "test_convert_special_numbers_579"() {
+        given:
+        NumConvertor convertor = new NumConvertor(5, 7, 9)
 
+        expect:
+        result == convertor.convert(number)
+
+        where:
+        number || result
+        1      || "1"
+        2      || "2"
+        3      || "3"
+        4      || "4"
+        5      || "石头"
+        6      || "6"
+        7      || "剪刀"
+        8      || "8"
+        9      || "布"
+        10     || "10"
+        13     || "13"
+        14     || "剪刀"
+        15     || "石头"
+        18     || "布"
+        21     || "21"
+        23     || "23"
+        33     || "33"
+        35     || "石头"
+        45     || "石头"
+        63     || "剪刀布"
+        105    || "石头"
+    }
 }
