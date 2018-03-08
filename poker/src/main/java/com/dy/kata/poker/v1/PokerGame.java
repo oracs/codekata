c022902a079dd9cda2c5049959f1c29f86dfaa2c
oracs <oracs@163.com>
2018-03-08 16:30:02 +0800

    #poker# using dsl to design rules

--- poker/src/main/java/com/dy/kata/poker/v1/PokerGame.java
+++ poker/src/main/java/com/dy/kata/poker/v1/PokerGame.java
@@ -5,7 +5,9 @@ public class PokerGame {
         Player player1 = parseHand(hand1);
         Player player2 = parseHand(hand2);
 
-        return "White wins. - with high card";
+        Result result = play1.compare(play2, rules(highcardRule()));
+
+        return String.format("%s wins. - with ", result.getWinner(), result.getReason());
     }
 
     private Player parseHand(String hand) {
