57c62d28333156a4b78ea56d1fe11b9e7d5fe2b8
oracs <oracs@163.com>
2018-03-17 12:25:30 +0800

    #string compare# add transformer impl

--- stringcompare/src/main/java/com/dy/kata/stringcompare/analyze/convertor/Transformer.java
+++ stringcompare/src/main/java/com/dy/kata/stringcompare/analyze/convertor/Transformer.java
@@ -1,10 +1,41 @@
 package com.dy.kata.stringcompare.analyze.convertor;
 
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 public class Transformer implements Convertor {
     @Override
-    public String convert(String str) {
-        String result = "";
+    public String convert(String input) {
+        StringBuilder result = new StringBuilder();
 
-        return null;
+        char[] charArray = input.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == '/') {
+                if (isNumeric(String.valueOf(charArray[i + 1]))) {  // after slash char is numeric
+                    int nextNumericCounts = calcNumericCounts(input, i + 1);
+                    result.append("#").append(input.substring(i + 1, i + 2 + nextNumericCounts)).append("#");
+                    i = i + nextNumericCounts + 1;
+                }
+            } else {
+                result.append(charArray[i]);
+            }
+        }
+
+        return result.toString();
     }
+
+    private int calcNumericCounts(String input, int position) {
+        int length = input.length();
+        int result = 0;
+
+        for (int i = 1; i < 3; i++) {
+            if ((position + i) < length && isNumeric(String.valueOf(input.charAt(position + i))) ) {
+                result = i;
+            }
+        }
+
+        return result;
+    }
+
+
 }
