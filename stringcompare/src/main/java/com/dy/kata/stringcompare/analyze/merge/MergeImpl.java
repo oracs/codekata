89836d1c9dac9aab68389ca25915fd10495e8c01
oracs <oracs@163.com>
2018-03-17 20:02:24 +0800

    #string compare# merge impl & refactoring test case

--- stringcompare/src/main/java/com/dy/kata/stringcompare/analyze/merge/MergeImpl.java
+++ stringcompare/src/main/java/com/dy/kata/stringcompare/analyze/merge/MergeImpl.java
@@ -1,10 +1,58 @@
 package com.dy.kata.stringcompare.analyze.merge;
 
 import com.dy.kata.stringcompare.model.SplitResult;
+import com.dy.kata.stringcompare.model.SplitUnit;
+
+import java.util.List;
 
 public class MergeImpl implements Merger {
     @Override
     public SplitResult merge(SplitResult input) {
-        return null;
+        List<SplitUnit> originalUnits = input.getSplitUnits();
+        SplitResult mergedResult = new SplitResult();
+
+        String mergedContent = originalUnits.get(0).content();
+        int mergedLength = originalUnits.get(0).length();
+        int size = originalUnits.size();
+        boolean flag = false;
+
+        // todo maybe perfermance issure.
+        for (int i = 0; i < size; i++) {
+            if (((i + 1) < size) && originalUnits.get(i).type() == originalUnits.get(i + 1).type()) {
+                mergedContent += originalUnits.get(i + 1).content();
+                mergedLength += originalUnits.get(i + 1).length();
+                flag = true;
+                continue;
+            }
+            if (flag) {
+                mergedResult.add(new SplitUnit(
+                        originalUnits.get(i).type(),
+                        mergedContent,
+                        mergedLength));
+
+                System.out.println("else new: " + mergedContent);
+
+                mergedContent = originalUnits.get(i).content();
+                mergedLength = originalUnits.get(i).length();
+                flag = false;
+            } else {
+                mergedResult.add(originalUnits.get(i));
+                System.out.println("differ: " + originalUnits.get(i).content());
+            }
+        }
+
+        if (flag) {
+            mergedResult.add(new SplitUnit(
+                    originalUnits.get(size - 1).type(),
+                    mergedContent,
+                    mergedLength));
+            System.out.println("last same:" + mergedContent);
+        }
+//            else {
+//                mergedResult.add(originalUnits.get(size - 1));
+//                System.out.println("last differ:" + originalUnits.get(size - 1).content());
+//            }
+
+        return mergedResult;
     }
 }
--- stringcompare/src/test/java/com/dy/kata/stringcompare/analyze/merge/MergeImplTest.java
+++ stringcompare/src/test/java/com/dy/kata/stringcompare/analyze/merge/MergeImplTest.java
@@ -1,29 +1,64 @@
 package com.dy.kata.stringcompare.analyze.merge;
 
+import com.dy.kata.stringcompare.analyze.helper.TestHelper;
+import com.dy.kata.stringcompare.model.ResultType;
 import com.dy.kata.stringcompare.model.SplitResult;
 import com.dy.kata.stringcompare.model.SplitUnit;
 import org.junit.Test;
 
+import java.util.List;
+
 import static com.dy.kata.stringcompare.model.ResultType.digit;
 import static com.dy.kata.stringcompare.model.ResultType.letter;
+import static java.util.Arrays.asList;
 
 public class MergeImplTest {
     @Test
     public void test_letter_merge() throws Exception {
-//        ("abc#123#456", 3, asList(letter, letter, digit), asList("abc","123","456"));
-        SplitUnit unit1 = new SplitUnit(letter, "abc", 3);
-        SplitUnit unit2 = new SplitUnit(letter, "123", 3);
-        SplitUnit unit3 = new SplitUnit(digit, "456", 3);
-
         SplitResult original = new SplitResult() ;
-        original.add(unit1);
-        original.add(unit2);
-        original.add(unit3);
 
+        original.add(new SplitUnit(letter, "abc", 3));
+        original.add(new SplitUnit(letter, "123", 3));
+        original.add(new SplitUnit(digit, "456", 3));
+
+        assertMergeResult(original, 2, asList(letter, digit), asList("abc123", "456"));
+    }
+
+    @Test
+    public void test_letter_differ() throws Exception {
+        SplitResult original = new SplitResult() ;
+
+        original.add(new SplitUnit(digit, "234", 3));
+        original.add(new SplitUnit(letter, "abc", 3));
+        original.add(new SplitUnit(digit, "456", 3));
+
+        assertMergeResult(original, 3, asList(digit, letter, digit), asList("234", "abc", "456"));
+    }
+
+    @Test
+    public void test_digit_merge() throws Exception {
+        SplitResult original = new SplitResult() ;
+
+        original.add(new SplitUnit(digit, "123", 3));
+        original.add(new SplitUnit(digit, "65", 1));
+        original.add(new SplitUnit(digit, "456", 3));
+        original.add(new SplitUnit(letter, "XYZ", 3));
+
+        assertMergeResultPro(original, 2,
+                asList(digit, letter),
+                asList("12365456", "XYZ"),
+                asList(7, 3));
+    }
+
+
+
+    public void assertMergeResult(SplitResult original, int expectSize, List<ResultType> resultTypes, List<String> contents) {
         SplitResult mergedResult = new MergeImpl().merge(original);
+        TestHelper.assertSplitResult(mergedResult, expectSize, resultTypes, contents);
+    }
 
-
-
-
+    public void assertMergeResultPro(SplitResult original, int expectSize, List<ResultType> resultTypes, List<String> contents, List<Integer> expectLengths) {
+        SplitResult mergedResult = new MergeImpl().merge(original);
+        TestHelper.assertSplitResultPro(mergedResult, expectSize, resultTypes, contents, expectLengths);
     }
 }
