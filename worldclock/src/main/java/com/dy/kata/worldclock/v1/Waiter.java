6b9f316427f1b25059b05112fc0a07dd85ad60a0
oracs <oracs@163.com>
2018-03-28 09:35:05 +0800

    refactoring

--- worldclock/src/main/java/com/dy/kata/worldclock/v1/Waiter.java
+++ worldclock/src/main/java/com/dy/kata/worldclock/v1/Waiter.java
@@ -3,9 +3,9 @@ package com.dy.kata.worldclock.v1;
 import com.dy.kata.worldclock.v1.impl.BeijingClock;
 
 public class Waiter {
-    public void syncTime(WallClocks wallclocks, BeijingClock adjTime) {
+    public void syncTime(WallClocks wallclocks, BeijingClock adjustTime) {
         for (Clock clock: wallclocks.clocks) {
-            clock.syncTime(adjTime);
+            clock.syncTime(adjustTime);
         }
     }
 }
